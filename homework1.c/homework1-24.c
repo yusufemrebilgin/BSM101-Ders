c3cac2773e12abfd174f941e90cc25c4eb19f0ce
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2022-12-04 22:39:04 +0300

    Update homework1-24.c

--- homework1.c/homework1-24.c
+++ homework1.c/homework1-24.c
@@ -4,37 +4,37 @@
 
 int main(){
 	
-	int sayi, sayac = 0, en_cok_tekrar_eden = 0;
+	int sayi , sayac , basamak_tekrari = 0 , en_cok_tekrar_eden = 0;
 	
-	printf("Bir sayi giriniz: ");
-	scanf("%d", &sayi);
+	printf("Bir sayi giriniz: ");	scanf("%d", &sayi);
 	
-	int x , y;
-	x = sayi;
+	int rakam , degisken;
 	
 	
-	while( x > 0 ){
+	while( sayi > 0 ){
 		
-		y = sayi % 10;
+		sayac = 0;
+		rakam = sayi % 10;
+		sayi = sayi / 10;
+		degisken = sayi;
 		
-		while( sayi > 0 ){
+		while( degisken > 0 ){
 			
-			if( sayi%10==y )
+			if( degisken%10==rakam ){
 				sayac++;
-			if( sayac >= en_cok_tekrar_eden )
-				en_cok_tekrar_eden = y;
-
-			sayi = sayi / 10;
+			}
+			if( sayac > basamak_tekrari ){
+				basamak_tekrari = sayac;
+				en_cok_tekrar_eden = rakam;
+			}
 			
-		}
-		
-		x = x / 10;
+			degisken = degisken / 10;
+		}	
 		
 	}
 	
-	printf("\nEn cok tekrar eden sayi: %d", en_cok_tekrar_eden);
-	
+	printf("\nEn cok tekrar eden: %d", en_cok_tekrar_eden);
+
 	return 0;
-	
 	
 }
