e8bec180a6051ddd2d540c9691de468915ea4fd5
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2022-12-15 11:48:44 +0300

    Update lab9-4.c

--- 9.Hafta/lab9-4.c
+++ 9.Hafta/lab9-4.c
@@ -5,23 +5,20 @@
 int main(){
 
 	int x, y, sayi, yenisayi=0;
-	
-	printf("Bir sayi giriniz: ");
-	scanf("%d", &sayi);
+	printf("Bir sayi giriniz: "); scanf("%d", &sayi);
 	
 	x = sayi;
 	
 	while( x>0 ){ 
     		y = x % 10;
-			yenisayi = y + ( yenisayi*10);
-			x = x / 10;
+		yenisayi = y + ( yenisayi*10);
+		x = x / 10;
 	} 
-             
-    if( sayi == yenisayi )
-    	printf("\nPalindromik sayidir.");
-    else	
-    	printf("\nPalindromik sayi degildir.");
+        
+	if( sayi == yenisayi )
+    		printf("\nPalindromik sayidir.");
+	else	
+    		printf("\nPalindromik sayi degildir.");
                     
  	return 0;  
-             
 }
