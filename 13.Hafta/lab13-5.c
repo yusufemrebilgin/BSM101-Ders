64f12571e93ef1b01d3299a81e67d5d76cfb2c19
yusufemrebilgin <87023953+yusufemrebilgin@users.noreply.github.com>
2023-01-04 10:46:43 +0300

    Create lab13-5.c

---
+++ 13.Hafta/lab13-5.c
@@ -0,0 +1,28 @@
+//Girilen cümleyi parametre olarak alýp içerisindeki kelime sayýsýný bulan fonksiyon yazýnýz. 
+#include<stdio.h>
+
+int kelimeSay(char[]);
+
+int main(){
+	
+	char cumle[100];
+	printf("Enter a sentence: ");
+	gets(cumle);
+	
+	printf("\nWord count: %d", kelimeSay(cumle));
+	
+	return 0;
+}
+
+int kelimeSay(char str[]){
+	
+	int i , j , kelime=0;
+	
+	for( i=0; str[i]!='\0' ; i++ )
+	
+		if(  (str[i]<=90 && str[i]>=65) || (str[i]<=122 && str[i]>=90)  )
+			if( (str[i+1]>=32 && str[i+1]<=64) || str[i+1]==0 )
+				kelime++;
+				
+	return kelime;
+}
